e2900511c7cfaaef0e970b1b61e52267120f48d5
nirewen <eduardolonderovieira@gmail.com>
2018-09-23 00:20:51 -0300

    - Added Normal category - Added partial support for subcommands - Added choose and dice to the Normal category - Fix double command name in logger function [Category.js]

---
+++ commands/normal/choose.js
@@ -0,0 +1,29 @@
+const RESPONSES = [
+	c => `Eu escolho **${c}**`,
+	c => `**${c}** é a melhor escolha`,
+	c => `**${c}** é a minha escolha`,
+	c => `**${c}**, é óbvio!`,
+	c => `Claro que **${c}**!`
+];
+
+export const desc = "Faz uma escolha pra você.";
+export const details = "Escolhe uma das opções aleatoriamente. 2 escolhas, no mínimo.";
+export const usage = "<escolha> | <escolha> [| escolhas...]";
+export const aliases = ['c', 'pick', 'decide'];
+export async function run(msg, suffix) {
+    if (!suffix)
+        return 'wrong usage';
+        
+    let choices = suffix.split(/ ?\| ?/);
+    if (choices.length < 2 && suffix.includes(','))
+        choices = suffix.split(/, ?/);
+
+    // remover vazias
+    choices = choices.filter(c => c !== '');
+
+    if (choices.length < 2)
+        return 'wrong usage';
+
+    let pick = ~~(Math.random() * choices.length);
+    msg.channel.send(RESPONSES[~~(Math.random() * RESPONSES.length)](choices[pick]));
+}
