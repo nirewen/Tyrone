0261fc95c944affe784020e66b62129aab0d4212
nirewen <eduardolonderovieira@gmail.com>
2018-10-17 01:29:23 -0300

    add UNO! as a winning message

--- events/message.js
+++ events/message.js
@@ -3,4 +3,9 @@ export default function(msg) {
         if (msg.content.toLowerCase().startsWith(this.categories[i].prefix))
             return this.categories[i].process(msg);
     }
+
+    if (msg.content === 'UNO!') {
+        let {uno} = this.categories.find(c => c.commands.hasOwnProperty('uno')).commands;
+        uno.task(msg, '!');
+    }
 }
