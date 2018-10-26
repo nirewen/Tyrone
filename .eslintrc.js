e0b70586b64d7783c83fb9ebced26db0038c10b6
nirewen <eduardolonderovieira@gmail.com>
2018-10-26 20:57:20 -0300

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "array-callback-return": 0,
+        "class-methods-use-this": 0,
+        "consistent-return": 0,
+        "import/extensions": 0,
+        "import/no-unresolved": 0,
+        "new-parens": 0,
+        "no-case-declarations": 0,
+        "no-cond-assign": 0,
+        "no-console": 0,
+        "no-empty": 0,
+        "no-unused-vars": 1,
+        "no-underscore-dangle": 0,
+    }
+};
