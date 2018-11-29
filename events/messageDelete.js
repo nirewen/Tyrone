38873ebfcd5248fee8dce28a53c5088fdb5aecb8
nirewen <eduardolonderovieira@gmail.com>
2018-11-29 20:22:31 -0200

    :bug: Check if response is set

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -3,6 +3,7 @@ import { Event } from '../structures/Event'
 export default class MessageDeleteEvent extends Event {
     async run (msg) {
         if (msg.command)
-            msg.response.delete()
+            if (msg.response)
+                msg.response.delete()
     }
 }
