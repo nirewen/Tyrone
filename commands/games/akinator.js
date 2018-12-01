53f2f8726b8a9b60df9a0831cd7adc61c1533082
nirewen <eduardolonderovieira@gmail.com>
2018-12-01 01:57:33 -0200

    Add Akinator game

---
+++ commands/games/akinator.js
@@ -0,0 +1,64 @@
+import { Akinator } from '../../games/Akinator'
+import { MessageEmbed } from 'discord.js'
+
+export const desc = 'Jogue Akinator pelo Discord'
+export async function run (msg) {
+    let game = this.bot.games.get('akinator').set(msg.author.id, new Akinator())
+    let question = await game.create()
+    let answers = ['Sim', 'Não', 'Não sei', 'Provavelmente sim', 'Provavelmente não']
+    let embed = new MessageEmbed()
+        .setAuthor('Akinator', 'https://raw.githubusercontent.com/nirewen/Tyrone/master/src/img/akinator_face.png')
+        .setTitle(question)
+        .setDescription(answers.map((answer, i) => `${i + 1}\u20E3 ${answer}`))
+        .setThumbnail('https://raw.githubusercontent.com/nirewen/Tyrone/master/src/img/akinator.png')
+        .setFooter(`${Math.floor(game.progression)}% ${game.progressionBar}`)
+        .setColor('#149EFF')
+
+    let message = await msg.send(embed)
+
+    msg.collector = message.createReactionCollector((r, u) => r.me && u.id === msg.author.id, { idle: 6E5 })
+
+    msg.collector.on('collect', async function (r, u) {
+        let answer = parseInt(r.emoji.name) - 1
+        if (r.emoji.name === '⏪')
+            question = await game.cancel()
+        else
+            question = await game.answer(answer)
+
+        if (question.expired) {
+            embed
+                .setTitle('')
+                .setDescription('O jogo expirou...')
+            
+            this.stop()
+        }
+
+        if (question.finished) {
+            embed
+                .setTitle(question.name)
+                .setDescription(question.description)
+                .setImage(question.image)
+                .setThumbnail()
+                .setFooter(`Ranking: ${question.ranking} • Resposta alcançada em ${game.step} jogadas`)
+            
+            this.stop()
+        } else
+            embed
+                .setTitle(question)
+                .setFooter(`${Math.floor(game.progression)}% ${game.progressionBar}`)
+
+        message.edit(embed)
+        r.users.remove(u)
+    })
+
+    msg.collector.on('end', () => {
+        this.bot.games.get('akinator').delete(msg.author.id)
+        
+        message.reactions.removeAll()
+    })
+
+    for (let [i] of answers.entries())
+        await message.react(i + 1 + '\u20E3')
+
+    await message.react('⏪')
+}
---
+++ games/Akinator.js
@@ -0,0 +1,122 @@
+import request from 'request-promise-native'
+import { xml2js as convert } from 'xml-js'
+
+const BASE = 'http://62.4.22.192:8166/ws/'
+
+import { Game } from './structures/Game'
+import { AbstractPlayer } from './structures/AbstractPlayer'
+
+export class Akinator extends Game {
+    get player () {
+        return this.queue[0]
+    }
+
+    addPlayer (member) {
+        if (!this.players[member.id]) {
+            let player = this.players[member.id] = new AbstractPlayer(member, this)
+
+            this.queue.push(player)
+            return player
+        } else
+            return null
+    }
+
+    async create () {
+        let { result: { parameters: { identification: { session, signature }, step_information: { step, question } } } } = await this.get('new_session.php')
+        this.session = session
+        this.signature = signature
+        this.step = step
+        this.progression = 0
+
+        return question
+    }
+
+    async answer (answer) {
+        let { step: currentStep } = this
+        let { result: { completion, parameters: { progression, question, step } } } = await this.get('answer.php', { step: currentStep, answer })
+
+        this.progression = progression
+        this.step = step
+
+        if (progression >= 95 || completion === 'KO - TIMEOUT')
+            return this.end()
+
+        return question
+    }
+
+    async cancel () {
+        let { step: currentStep } = this
+        let { result: { completion, parameters: { progression, question, step } } } = await this.get('cancel_answer.php', { step: currentStep })
+
+        this.progression = progression
+        this.step = step
+
+        if (progression >= 95 || completion === 'KO - TIMEOUT')
+            return this.end()
+
+        return question
+    }
+
+    async end () {
+        let { step } = this
+        let { result: { completion, parameters: { elements: { element: { absolute_picture_path: image, description, ranking, name } } } } } = await this.get('list.php', { step, size: 1 })
+
+        if (completion === 'KO - ELEM LIST IS EMPTY')
+            return { expired: true }
+
+        return { finished: true, image, description, ranking, name }
+    }
+
+    get progressionBar () {
+        let percentage = Math.round(this.progression / 10)
+        let remaining = 10 - percentage
+
+        return `${'█'.repeat(percentage)}${'▁'.repeat(remaining)}`
+    }
+
+    get (path, qs = {}) {
+        qs.partner = 410
+        qs.constraint = "ETAT<>'AV'"
+        qs.session = this.session
+        qs.signature = this.signature
+
+        return request({ 
+            url: BASE + path, 
+            qs, 
+            transform: body => convert(body, { 
+                compact: true,
+                trim: true,
+                ignoreDeclaration: true,
+                ignoreInstruction: true,
+                ignoreAttributes: true,
+                ignoreComment: true,
+                ignoreCdata: true,
+                ignoreDoctype: true, 
+                elementNameFn: val => val.toLowerCase(), 
+                textFn: (value, parentElement) => {
+                    try {
+                        let keyNo = Object.keys(parentElement._parent).length
+                        let keyName = Object.keys(parentElement._parent)[keyNo - 1]
+                        parentElement._parent[keyName] = this.nativeType(value)
+                    } catch (e) {}
+                }
+            }) 
+        })
+    }
+
+    nativeType (value) {
+        let nValue = Number(value)
+
+        if (!isNaN(nValue))
+            return nValue
+            
+        let bValue = value.toLowerCase()
+
+        if (bValue === 'true')
+            return true
+        else if (bValue === 'false')
+            return false
+        
+        return value
+    }
+}
