41bbe32832faa2e00414f4cd74e4fa01dcee8ffb
nirewen <eduardolonderovieira@gmail.com>
2018-12-03 01:41:28 -0200

    Fix reload of subcommands

--- events/reload.js
+++ events/reload.js
@@ -10,7 +10,7 @@ export async function run (path) {
 
         category = this.categories.find(c => path.join('/').startsWith(c.dir))
 
-        let command = reload(join(...['..', ...path]))
+        let command = reload(join('..', folder, category, name + '.js'))
 
         if (category.commands.has(name) && command.run)
             category.commands.set(name, new Command(name, category.prefix, command, category, this))
@@ -20,7 +20,7 @@ export async function run (path) {
     if (folder === 'events') {
         let [name] = subfolders.map(e => e.split('.')[0])
 
-        let Event = reload(join(...['..', ...path]))
+        let Event = reload(join('..', ...path))
 
         if (Event.run)
             this.events.set(name, Event.run)
@@ -28,20 +28,20 @@ export async function run (path) {
     }
 
     if (folder === 'config.json')
-        this.config = reload(join(...['..', ...path]))
+        this.config = reload(join('..', ...path))
 
     if (folder === 'structures') {
         let [structure] = subfolders.map(s => s.split('.')[0])
 
         if (structure === 'Category') {
-            let { Category } = reload(join(...['..', ...path]))
+            let { Category } = reload(join('..', ...path))
 
             this.loadCommandSets(Category)
             this.initCategories()
         }
 
         if (structure === 'Logger') {
-            let { Logger } = reload(join(...['..', ...path]))
+            let { Logger } = reload(join('..', ...path))
 
             this.logger = new Logger()
             this.categories.each(c => {
