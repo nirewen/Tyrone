7dc7111fc4a938cb78d4bf8299d827d047ec12a8
nirewen <eduardolonderovieira@gmail.com>
2018-12-03 20:51:47 -0200

    Delete response if message has attachments

--- structures/lib/Message.js
+++ structures/lib/Message.js
@@ -9,26 +9,27 @@ const getMatches = (string, regex) => {
     return matches
 }
 
-export const Message = DiscordMessage => { 
+export const Message = DiscordMessage => {
     class Message extends DiscordMessage {
         async send (content, options) {
             if (this.response) {
                 if (this.response.attachments.size) {
                     await this.response.delete()
+                    delete this.response
                     return this.send(content, options)
                 }
-    
+
                 if (this.collector)
                     this.collector.stop()
-    
+
                 if (!(content instanceof MessageEmbed))
                     content = { content, embed: null }
-    
+
                 return this.response.edit(content, options)
             }
-    
+
             this.response = await this.channel.send(content, options)
-    
+
             return this.response
         }
 
@@ -36,7 +37,7 @@ export const Message = DiscordMessage => {
             let flags = new Map()
 
             Object.defineProperty(flags, 'regex', {
-                get: () => /--(\w+)\s??(.+?(?=--|$))?/g, 
+                get: () => /--(\w+)\s??(.+?(?=--|$))?/g,
                 enumerable: false
             })
 
