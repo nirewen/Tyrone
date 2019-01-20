c6b7f681baa797af106156d84de4676e4efe12c4
nirewen <eduardolonderovieira@gmail.com>
2019-01-20 13:42:42 -0200

    Create serverinfo

---
+++ commands/normal/serverinfo.js
@@ -0,0 +1,47 @@
+import { MessageEmbed, Util } from 'discord.js'
+import { format, regions, regionEmojis } from './serverinfo.utils/utils'
+import moment from 'moment'
+
+export const desc = 'Exibe um card de informações do servidor'
+export const guildOnly = true
+export const aliases = ['server', 'si']
+export async function run (msg) {
+    const findEmoji = name => this.bot.emojis.find(e => e.name === name && e.guild.id === '199330631061078017')
+    let { name, id, icon, members, region, roles, channels, owner, joinedAt, createdAt } = msg.guild
+    let guildIcon = icon ? msg.guild.iconURL({ format: 'png', size: 2048 }) : `https://guild-default-icon.herokuapp.com/${msg.guild.nameAcronym}`
+    let statuses = msg.guild.members.reduce((c, n) => {
+        c[n.presence.status] = (c[n.presence.status] || 0) + 1
+
+        if (n.user.bot)
+            c.bots = (c.bots || 0) + 1
+        else
+            c.users = (c.users || 0) + 1
+
+        return c
+    }, {})
+    let regionEmoji = Object.entries(regionEmojis).find(([e, v]) => v.includes(region))[0]
+    let serverRegion = regions[region]
+
+    msg.send(new MessageEmbed()
+        .setTitle(`${findEmoji('discord')} ${Util.escapeMarkdown(name)}`)
+        .setThumbnail(guildIcon)
+        .addField(':snowflake: ID', id, true)
+        .addField(':crown: Dono', owner, true)
+        .addField(`${regionEmoji} Região`, serverRegion, true)
+        .addField(':speech_balloon: Canais', [
+            `${findEmoji('text')}` + channels.filter(c => c.type === 'text').size,
+            `${findEmoji('voice')}` + channels.filter(c => c.type === 'voice').size
+        ], true)
+        .addField(':date: Criado em', moment(createdAt).format(format))
+        .addField(':star2: Entrei aqui em', moment(joinedAt).format(format))
+        .addField(`:busts_in_silhouette: Membros [${members.size}]`, [
+            `${findEmoji('online')} ${statuses.online || 0}\t\t`,
+            `${findEmoji('idle')} ${statuses.idle || 0}\t\t`,
+            `${findEmoji('dnd')} ${statuses.dnd || 0}\t\t`,
+            `${findEmoji('offline')} ${statuses.offline || 0}\n`,
+            `:man_in_tuxedo: **Pessoas**: ${members.filter(m => !m.user.bot).size}\n`,
+            `:robot: **Bots**: ${members.filter(m => m.user.bot).size}`
+        ].join(''))
+        .addField(`:briefcase: Cargos [${roles.size}]`, roles.size > 0 ? Util.discordSort(roles).map(r => r).reverse().slice(0, 15).join(', ') : 'Nenhum')
+        .setColor(owner.displayColor))
+}
---
+++ commands/normal/serverinfo.utils/utils.js
@@ -0,0 +1,23 @@
+export const regions = {
+    'amsterdam' : ':flag_nl: Amsterdã',
+    'brazil'    : ':flag_br: Brasil',
+    'eu-central': ':flag_eu: Europa Central',
+    'eu-west'   : ':flag_eu: Europa Ocidental',
+    'frankfurt' : ':flag_de: Frankfurt',
+    'hongkong'  : ':flag_hk: Hong Kong',
+    'japan'     : ':flag_ja: Japão',
+    'london'    : ':flag_gb: Londres',
+    'russia'    : ':flag_ru: Russia',
+    'singapore' : ':flag_sg: Singapura',
+    'sydney'    : ':flag_au: Sydney',
+    'us-central': ':flag_us: EUA Central',
+    'us-east'   : ':flag_us: EUA Oriental',
+    'us-west'   : ':flag_us: EUA Ocidental',
+    'us-south'  : ':flag_us: EUA Sul'
+}
+export const regionEmojis = {
+    ':earth_americas:': ['us-central', 'us-east', 'brazil', 'us-south', 'us-west'],
+    ':earth_africa:'  : ['eu-central', 'eu-west', 'london', 'amsterdam', 'frankfurt'],
+    ':earth_asia:'    : ['hongkong', 'russia', 'singapore', 'sydney', 'japan']
+}
+export const format = 'D [de] MMMM [de] YYYY, [às] HH:mm'
