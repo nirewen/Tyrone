626de57044872f7b42507a2942f285319b77b389
nirewen <eduardolonderovieira@gmail.com>
2020-05-13 00:44:32 -0300

    Fix ready

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 export async function run () {
-    let { users: { size: users }, guilds: { size: guilds } } = this
+    let { users: { cache: { size: users } }, guilds: { cache: { size: guilds } } } = this
     
     this.logger.logWithHeader('PRONTO', 'bgGreen', 'white', `S: ${guilds} | U: ${users} | MÉD: ${(users / guilds).toFixed(2)} | PID: ${process.pid}`)
 }
