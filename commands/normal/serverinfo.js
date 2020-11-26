9d43bdbaff974892d012ba67634d6970b93c6bb6
nirewen <eduardolonderovieira@gmail.com>
2020-11-25 23:27:14 -0300

    This is useless now

--- commands/normal/serverinfo.js
+++ commands/normal/serverinfo.js
@@ -7,7 +7,7 @@ export const guildOnly = true
 export const aliases = ['server', 'si']
 export async function run (msg) {
     const findEmoji = name => this.bot.emojis.cache.find(e => e.name === name && e.guild.id === '199330631061078017')
-    let { name, id, icon, members: { cache: members }, region, roles: { cache: roles }, channels: { cache: channels }, ownerID, joinedAt, createdAt } = msg.guild
+    let { name, id, icon, region, roles: { cache: roles }, channels: { cache: channels }, ownerID, joinedAt, createdAt } = msg.guild
     let guildIcon = icon ? msg.guild.iconURL({ format: 'png', size: 2048 }) : `https://guild-default-icon.herokuapp.com/${msg.guild.nameAcronym}`
     // let statuses = members.reduce((c, n) => {
     //     c[n.presence.status] = (c[n.presence.status] || 0) + 1
